2d4f2d07c70d706b6075f2757cdd23727ceb06b7
Damien <48095764+CdPlagiat@users.noreply.github.com>
2019-11-22 17:16:33 +0100

    Add files via upload

---
+++ Catalogue.cpp
@@ -0,0 +1,76 @@
+/*************************************************************************
+                           Catalogue  -  description
+                             -------------------
+    début                : 22/11/2019
+    copyright            : (C) 2019-2020 par CARREAU Damien
+    e-mail               : carreau.damien@gmail.com
+*************************************************************************/
+
+//---------- Réalisation de la classe <Catalogue> (fichier Catalogue.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+#include <ctring>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Catalogue.h"
+#include "Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+void Reallocation( Liste* liste )
+// Algorithme : Réalloue un espace plus grand pour le tableau de trajets
+//
+{
+	trajets->allocation+=10;
+	tab* = new Trajet[trajets->allocation];
+	for(int i = 0 ; i < trajets->taille ; i++){
+		tab[i] = trajets->tableau[i];
+	}
+	delete trajets->tableau;
+	trajets->tableau = tab;
+} //----- Fin de Reallocation
+
+//-------------------------------------------- Constructeurs - destructeur;
+
+Catalogue::Catalogue ( )
+// Algorithme :
+//
+{
+	#ifdef MAP
+    		cout << "Appel au constructeur de <Catalogue>" << endl;
+	#endif
+	
+	trajets->allocation = 10;
+	trajets->taille = 0;
+	trajets->tableau = new Trajet[trajets->allocation];
+} //----- Fin de Catalogue
+
+
+Catalogue::~Catalogue ( )
+// Algorithme :
+//
+{
+	#ifdef MAP
+    		cout << "Appel au destructeur de <Catalogue>" << endl;
+	#endif
+
+	for(int i = 0; i < trajets->taille ; i++ ){
+		delete trajets->tableau[i];
+	}
+	delete[] trajets->tableau;
+	delete trajets;
+} //----- Fin de ~Catalogue
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
