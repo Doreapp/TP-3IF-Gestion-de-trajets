052afb93642b07b926ca1b426faa402cab2f4d97
Damien <48095764+CdPlagiat@users.noreply.github.com>
2019-11-29 17:56:15 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ using namespace std;
 #include "Trajet.h"
 #include "TrajetSimple.h"
 #include "TrajetCompose.h"
+#include "TrajetListe.h"
 
 //-------------------------------------------------------------------Constantes
 
@@ -53,9 +54,8 @@ int main()
 	while(strcmp(lecture,"Quitter")!=0){
 		if(strcmp(lecture,"Ajout")==0){
 			cout << "Entrez 0 pour ajouter un trajet simple, 1 sinon\n";
-			int choix;
-			cin >> choix;
-			if(choix == 0){
+			cin >> lecture;
+			if(!strcmp(lecture,"0")){
 				cout << "Entrez votre ville de départ\n";
 				char * ville_depart = new char[100];
 				cin >> ville_depart;
@@ -71,14 +71,20 @@ int main()
 				delete ville_depart;
 				delete ville_arrive;
 				delete moyen;
-			}else if(choix == 1){
+			}else if(!strcmp(lecture,"1")){
 				TrajetCompose * trajet = new TrajetCompose();
 				char * ville_depart = new char[100];
 				char * ville_arrive = new char[100];
 				char * moyen = new char[100];
+
+				cout << "Entrez votre ville de départ\n";
+				cin >> ville_depart;
+
 				do{
-					cout << "Entrez votre ville de départ\n";
-					cin >> ville_depart;
+					if(!strcmp(lecture,"0")){
+							strcpy(ville_depart,ville_arrive);
+					}
+
 					cout << "Entrez votre ville d'arrivé\n";
 					cin >> ville_arrive;
 					cout << "Entrez votre moyen de transport\n";
@@ -86,15 +92,17 @@ int main()
 					
 					trajet->Ajoute(new TrajetSimple(ville_depart,ville_arrive,moyen));
 
-					cout << "Entrez 1 pour ajouter une nouvelle étape, 0 sinon\n";
-					cin >> choix;
-				}while(choix == 1);
+					cout << "Entrez 0 pour ajouter une nouvelle étape, 1 sinon\n";
+					cin >> lecture;
+				}while(!strcmp(lecture,"0"));
 
 				catalogue->Ajoute(trajet);
 				cout << "Votre trajet a bien été ajouté\n";
 				delete ville_depart;
 				delete ville_arrive;
 				delete moyen;
+			}else{
+				cout << "Erreur entrée\n";
 			}		
 		}else if(strcmp(lecture,"Affichage")==0){
 			catalogue->Affiche();
@@ -104,10 +112,21 @@ int main()
 			cin >> ville_depart;
 			cout << "Entrez votre ville d'arrivé\n";
 			cin >> lecture;
-			const Trajet * recherche = catalogue->TrouveTrajet(ville_depart,lecture);
-			if(recherche!=nullptr)
-				recherche->Affiche();
-			cout << "\n";
+			const TrajetListe * recherche = catalogue->TrouveTrajet(ville_depart,lecture);
+			if(recherche->Vide()){
+				cout << "Trajet non trouvé";
+			}else{
+				Element* curr = recherche->PremierElement();
+				int i = 1;
+				while(curr != nullptr){
+					cout << i << ":";
+					curr->contenu->Affiche();
+					i++;
+					curr = curr->suivant;
+					cout << endl;
+				}
+				delete curr;
+			}
 			delete ville_depart;
 		}
 		cin >> lecture;
