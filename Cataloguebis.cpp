90a575958df34eda3be8507dc03c9a45d29fd457
Damien <48095764+CdPlagiat@users.noreply.github.com>
2019-11-30 13:53:53 +0100

    Add files via upload

---
+++ Cataloguebis.cpp
@@ -0,0 +1,147 @@
+/*************************************************************************
+						   Catalogue  -  description
+							 -------------------
+	début                : 22/11/2019
+	copyright            : (C) 2019-2020 par CARREAU Damien
+	e-mail               : carreau.damien@gmail.com
+*************************************************************************/
+
+//---------- Réalisation de la classe <Catalogue> (fichier Catalogue.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Catalogue.h"
+#include "Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+void Catalogue::Ajoute(const Trajet* trajet)
+// Algorithme : Ajoute un trajet au catalogue
+//
+{
+	trajets->Ajouter(trajet);
+}
+// ------ Fin de Ajoute
+
+const TrajetListe* Catalogue::TrouveTrajet(const char* depart, const char* arrivee) const
+// Algorithme : Trouve les trjats possibles pour aller de la ville de depart jusqu a la ville d arrivee
+//
+{
+	TrajetListe* res = new TrajetListe();
+
+	if (trajets->Vide()) {
+		cout << "Catalogue vide\n";
+		return res;
+	}
+	
+	Element* curr = trajets->PremierElement();
+	while (curr != nullptr) {
+		if (!strcmp(curr->contenu->GetVilleDepart(), depart) && 
+			!strcmp(curr->contenu->GetVilleArrivee(), arrivee))
+			res->Ajouter(curr->contenu);
+
+		curr = curr->suivant;
+	}
+
+	return res;
+}
+// ----- Fin de trouve trajet
+
+
+const TrajetListe* Catalogue::Recherche(const char* depart, const char* arrivee) const
+// Algorithme : Recherche 2
+//
+{
+	TrajetListe* res = new TrajetListe();
+
+	if(trajets->Vide()){
+		cout << "Catalogue vide\n";
+		return res;
+	}	
+	
+	Element* curr = trajets->PremierElement();
+	while (curr != nullptr){
+		if (!strcmp(curr->contenu->GetVilleDepart(), depart) && 
+			!strcmp(curr->contenu->GetVilleArrivee(), arrivee))
+			res->Ajouter(curr->contenu);
+		else if(!strcmp(curr->contenu->GetVilleDepart(),depart)){
+			const TrajetListe* inter = Recherche(curr->contenu->GetVilleArrivee(),arrivee);
+			if(!inter->Vide()){
+				res->Ajouter(curr->contenu);
+				Element* elem = inter->PremierElement();
+				while(elem != nullptr){
+					res->Ajouter(elem->contenu);
+					elem = elem->suivant;
+				}
+			}
+		}
+		curr = curr->suivant;
+	}
+	return res;
+}
+// ---- Fin de Recherche
+
+
+const void Catalogue::Affiche() const
+// Algorithme : Affiche les trajets du catalogue dans le terminal
+//
+{
+	if (trajets->Vide()) {
+		cout << "Catalogue vide";
+		return;
+	}
+
+	Element* curr = trajets->PremierElement();
+	int i = 1;
+	while (curr != nullptr) {
+		cout << i << " : ";
+		curr->contenu->Affiche();
+		i++;
+
+		curr = curr->suivant;
+		cout << endl;
+	}
+}
+// ----- Fin de Affichage
+
+//----------------------------------------------------- Méthodes privés
+
+//-------------------------------------------- Constructeurs - destructeur;
+
+Catalogue::Catalogue()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <Catalogue>" << endl;
+#endif
+
+	trajets = new TrajetListe();
+} //----- Fin de Catalogue
+
+
+Catalogue::~Catalogue()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au destructeur de <Catalogue>" << endl;
+#endif
+
+	delete trajets;
+} //----- Fin de ~Catalogue
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
---
+++ mainbis.cpp
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ 			  main - description
+			  ------------------
+
+	début		: 22/11/2019
+	copyright            : (C) 2019/2020 par Carreau Damien et Mandin Antoine
+	e-mail               : antoine.mandin@insa-lyon.fr / damien.carreau@insa-lyon.fr
+********************************************************************************/
+
+//------------Réalisation de la classe <main> (fichier main.cpp) ---------------
+
+//----------------------------------------------------------------------INCLUDE
+
+//--------------------------------------------------------------Include système
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//------------------------------------------------------------Include personnel
+#include "Catalogue.h"
+#include "Trajet.h"
+#include "TrajetSimple.h"
+#include "TrajetCompose.h"
+#include "TrajetListe.h"
+
+//-------------------------------------------------------------------Constantes
+
+//-----------------------------------------------------------------------PUBLIC
+
+//-----------------------------------------------------------Méthodes publiques
+
+//--------------------------------------------------------------Méthodes privés
+
+//--------------------------------------------------Constructeurs - destructeur
+
+int main()
+// Algoritme : permet d'excuter les fonctionalités du code
+// Interface homme code
+//
+{
+	char * lecture = new char[100];
+	Catalogue * catalogue = new Catalogue();
+	
+	Trajet * ts1 = new TrajetSimple("Lyon","Paris","Voiture");
+	catalogue->Ajoute(ts1);
+	Trajet * ts2 = new TrajetSimple("Paris","Lille","Train");
+	catalogue->Ajoute(ts2);
+	TrajetCompose * tc1 = new TrajetCompose();
+	tc1->Ajoute(new TrajetSimple("Nimes","Lyon","Bus"));
+	tc1->Ajoute(new TrajetSimple("Lyon","Grenoble","Voiture"));
+	catalogue->Ajoute(tc1);
+	
+	cout << "Bienvenue dans le menu, vous avez accés à différentes fonctionnalitées\n     Ajout - permet d'ajouter un trajet à votre catalogue\n     Affichage - permet d'afficher votre catalogue\n     Recherche - retourne les trajets du catalogue allant d'une ville A à une autre B\n     Quitter - Clos le programme\n";
+
+	cin >> lecture;
+	while(strcmp(lecture,"Quitter")!=0){
+		if(strcmp(lecture,"Ajout")==0){
+			cout << "Entrez 0 pour ajouter un trajet simple, 1 sinon\n";
+			cin >> lecture;
+			if(!strcmp(lecture,"0")){
+				cout << "Entrez votre ville de départ\n";
+				char * ville_depart = new char[100];
+				cin >> ville_depart;
+				cout << "Entrez votre ville d'arrivé\n";
+				char * ville_arrive = new char[100];
+				cin >> ville_arrive;
+				cout << "Entrez votre moyen de transport\n";
+				char * moyen = new char[100];
+				cin >> moyen;
+				Trajet * trajet = new TrajetSimple(ville_depart,ville_arrive,moyen);
+				catalogue->Ajoute(trajet);
+				cout << "Votre trajet a bien été ajouté\n";	
+				delete ville_depart;
+				delete ville_arrive;
+				delete moyen;
+			}else if(!strcmp(lecture,"1")){
+				TrajetCompose * trajet = new TrajetCompose();
+				char * ville_depart = new char[100];
+				char * ville_arrive = new char[100];
+				char * moyen = new char[100];
+
+				cout << "Entrez votre ville de départ\n";
+				cin >> ville_depart;
+
+				do{
+					if(!strcmp(lecture,"0")){
+							strcpy(ville_depart,ville_arrive);
+					}
+
+					cout << "Entrez votre ville d'arrivé\n";
+					cin >> ville_arrive;
+					cout << "Entrez votre moyen de transport\n";
+					cin >> moyen;
+					
+					trajet->Ajoute(new TrajetSimple(ville_depart,ville_arrive,moyen));
+
+					cout << "Entrez 0 pour ajouter une nouvelle étape, 1 sinon\n";
+					cin >> lecture;
+				}while(!strcmp(lecture,"0"));
+
+				catalogue->Ajoute(trajet);
+				cout << "Votre trajet a bien été ajouté\n";
+				delete ville_depart;
+				delete ville_arrive;
+				delete moyen;
+			}else{
+				cout << "Erreur entrée - retour menu\n";
+			}		
+		}else if(strcmp(lecture,"Affichage")==0){
+			catalogue->Affiche();
+		}else if(strcmp(lecture,"Recherche")==0){
+			char * ville_depart = new char[100];
+			char * ville_arrive = new char[100];
+			cout << "Entrez votre ville de départ\n";
+			cin >> ville_depart;
+			cout << "Entrez votre ville d'arrivé\n";
+			cin >> ville_arrive;
+			cout << "Souhaitez vous faire une recherche simple 0, ou une avancée 1\n";
+			cin >> lecture;
+			const TrajetListe * recherche;
+			if(!strcmp(lecture,"0")){
+				recherche = catalogue->TrouveTrajet(ville_depart,ville_arrive);
+			}else if(!strcmp(lecture,"1")){
+				recherche = catalogue->Recherche(ville_depart,ville_arrive);
+			}
+			if(recherche->Vide()){
+				cout << "Trajet non trouvé\n";
+			}else{
+				Element* curr = recherche->PremierElement();
+				int i = 1;
+				while(curr != nullptr){
+					cout << i << ":";
+					curr->contenu->Affiche();
+					i++;
+					curr = curr->suivant;
+					cout << endl;
+				}
+				delete curr;
+			}
+			delete ville_depart;
+			delete ville_arrive;
+			delete recherche;
+		}
+		cin >> lecture;
+	}	
+	delete catalogue;
+	return 0;
+}
+// ------ Fin de Affichage
+
+//-----------------------------------------------------------------------PRIVE
+
+//----------------------------------------------------------Méthodes protégées
