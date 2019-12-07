cd3ee00bbfb3d38dbd6cffdcbf0fb551646eeb25
Doreapp <55652808+Doreapp@users.noreply.github.com>
2019-12-07 15:43:40 +0100

    Commentaires +

--- Catalogue.cpp
+++ Catalogue.cpp
@@ -29,8 +29,7 @@ void Catalogue::Ajoute(const Trajet* trajet)
 // Ajoute {trajet} au catalogue (à la liste de trajets)
 {
 	trajets->Ajouter(trajet);
-}
-// ------ Fin de Ajoute
+}// ------ Fin de Ajoute
 
 const TrajetListe* Catalogue::TrouveTrajet(const char* depart, const char* arrivee) const
 // Algorithme : 
@@ -52,15 +51,14 @@ const TrajetListe* Catalogue::TrouveTrajet(const char* depart, const char* arriv
 	}
 
 	return res;
-}
-// ----- Fin de trouve trajet
+}// ----- Fin de trouve trajet
 
 
 const TrajetListe* Catalogue::Recherche(const char* depart, const char* arrivee) const
 // Algorithme : 
 // Recherche avancée = Cherche toutes les possibles compositions de trajets permettant
 // d'arriver à la ville {arrivee} en partant de la ville {depart}
-// -> Voir RechercheDepart pour l'algorithme récursif utilisé
+// -> Voir [RechercheDepart] pour l'algorithme récursif utilisé
 // Affiche 'Catalogue Vide' si la liste des trajets est vide
 {
 	//Liste à remplir des trajets possibles
@@ -74,44 +72,40 @@ const TrajetListe* Catalogue::Recherche(const char* depart, const char* arrivee)
 	RechercheDepart(depart, arrivee, new TrajetListe(trajets), res);
 
 	return res;
-}
-// ---- Fin de Recherche
+}// ---- Fin de Recherche
 
 
 void Catalogue::RechercheDepart(const char* depart, const char* arrivee, TrajetListe* into, 
 	TrajetListe* res, TrajetListe* debut) const 
 // Algorithme :
-// 1) Nettoye la liste {into} des trajets finissant à la ville {arrivee} (pas de boucle)
+// 1) Nettoye la liste {into} des trajets finissant à la ville {arrivee} (interdit les boucles)
 // 2) Pour chaques trajets de {into}
-//		S'il commence par {depart}
-//		a) s'il finit par {arrivee}, l'ajouter à la liste {res}
-//		b) sinon appeler RechercheDepart avec {depart} vallant la ville d'arrivée du trajet en question
+//		S'il commence par {depart} :
+//			a) s'il finit par {arrivee}: l'ajouter à la liste {res}
+//			b) sinon: appeler RechercheDepart avec {depart} vallant la ville d'arrivée du trajet en question
 {
 	// Supprimer ceux qui finissent par depart :
-	// taille de la liste
 	int const length(into->Taille());
-	// indices de la liste à supprimés
 	int* indexes = new int[length];
-	// ittérateur
 	int i;
 
 	// remplissage du tableau de -1
 	for (i = 0; i < into->Taille(); i++)
 		indexes[i] = -1;
 	
-	// Ressence les indices de trajets finissant par depart (à supprimer)
+	// Trouve les indices de trajets finissant par depart (à supprimer)
 	i = 0;
 	int j = 0;
 	for (const Trajet* t : *into) {
 		if (!strcmp(t->GetVilleArrivee(), depart) && i < length) {
 			indexes[i++] = j--; //enregistre puis décrémente j, 
 			//car une fois une fois un trajet supprimé, le trajet suivant à supprimer
-			//sera 1 indice plus avant et de même pour les suivants
+			//sera 1 indice plus tôt dans la liste et de même pour les suivants
 		}
 		j++;
 	}
 
-	//	Supprimer réellement les trajets
+	// Supprime réellement les trajets
 	for (i = 0; i < length && indexes[i] != -1; i++) {
 		into->Supprimer(indexes[i]);
 	}
@@ -121,19 +115,14 @@ void Catalogue::RechercheDepart(const char* depart, const char* arrivee, TrajetL
 	//Pour chaque trajet de {into}
 	for (const Trajet* trajet : *into) 
 	{
-		// S'il commence par {depart}
 		if (!strcmp(trajet->GetVilleDepart(), depart)) 
 		{
-			// s'il fini par {arrivee}
 			if (!strcmp(trajet->GetVilleArrivee(), arrivee)) 
 			{
-				// On l'ajoute 
 				if (debut == nullptr) { // si debut null (pas de trajet avant necessaire pour arriver à {depart})
 					res->Ajouter(trajet);
 				}
 				else { //sinon ajoute un trajet composé vallant {debut + trajet}
-					//TrajetCompose* found = new TrajetCompose(debut);
-					//found->Ajoute(trajet);
 					for (const Trajet* t : *debut)
 						res->Ajouter(t);
 					res->Ajouter(trajet);
@@ -159,10 +148,10 @@ void Catalogue::RechercheDepart(const char* depart, const char* arrivee, TrajetL
 	if (debut != nullptr)
 		delete debut;
 	delete into;
-}
+}// ----- Fin de RechercheDepart
 
 
-const void Catalogue::Affiche() const
+void Catalogue::Affiche() const
 // Algorithme : 
 // Affiche les trajets du catalogue dans le terminal
 {
